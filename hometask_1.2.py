42df7b2b35ce3f23a1e42cbe99f37e7894db967e
Svetabtr <sveta-btr@mail.ru>
2022-09-25 16:16:44 +0300

    Добавлено решение по задаче 1.2

--- hometask_1.2.py
+++ hometask_1.2.py
@@ -1,2 +1,10 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+X = 1
+Y = 1
+Z = 1
+
+res1 = bool(not (X or Y or Z))
+res2 = bool(not X and not Y and not Z)
+
+print(bool(res1== res2))
