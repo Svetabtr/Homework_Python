18d8f57203669a125a2d7f6593149c2f6e922927
Svetabtr <sveta-btr@mail.ru>
2022-10-01 22:32:59 +0300

    Дз 2

--- hometask_2.4.py
+++ hometask_2.4.py
@@ -1,2 +1,33 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+import random
+
+lst = [random.randint(-10, 10) for i in range(100)]
+print(lst)
+
+mult = 1
+data = open('file.txt', 'r')
+for line in data:
+    line = int(data.readline())
+    mult *= lst[line]
+    print(lst[line])
+print('Произведение чисел = ', mult)
+
+print(lst[45], lst[20], lst[4], lst[92], lst[10])
+#sum=0
+
+# id - взять сам элемент - id[i]
+# lst - взять индекс равный элементу - 
+
+# for i in id:
+#     a=lst[i]
+
+#     if id[i] == lst[a]:
+#         sum *= id[i]
+    
+# for line in data:
+#     if int(data.readline()) == lst.index(int(data.readline())):
+#         print('yes')
+#     else:
+#         print('no')
