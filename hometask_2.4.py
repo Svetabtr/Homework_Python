84d4ad5448122a1fe2a302d5669449a1ab77939d
Svetabtr <sveta-btr@mail.ru>
2022-10-02 13:38:34 +0300

    исправления по задаче 2.4

--- hometask_2.4.py
+++ hometask_2.4.py
@@ -3,31 +3,17 @@
 
 import random
 
-lst = [random.randint(-10, 10) for i in range(100)]
+lst = [random.randint(-50, 50) for i in range(100)]
+
 print(lst)
 
 mult = 1
 data = open('file.txt', 'r')
+
 for line in data:
-    line = int(data.readline())
+    line = int(line)
     mult *= lst[line]
-    print(lst[line])
+   
 print('Произведение чисел = ', mult)
-
-print(lst[45], lst[20], lst[4], lst[92], lst[10])
-#sum=0
-
-# id - взять сам элемент - id[i]
-# lst - взять индекс равный элементу - 
-
-# for i in id:
-#     a=lst[i]
-
-#     if id[i] == lst[a]:
-#         sum *= id[i]
-    
-# for line in data:
-#     if int(data.readline()) == lst.index(int(data.readline())):
-#         print('yes')
-#     else:
-#         print('no')
+data.close()
+# print(lst[45], lst[20], lst[4], lst[92], lst[10])
